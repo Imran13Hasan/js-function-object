b843f8b74d1dfc00d829da0239f9edeade91810a
Imran Hasan <imran13hasan@gmail.com>
2023-04-11 12:35:10 +0600

    function

---
+++ parameter.js
@@ -0,0 +1,16 @@
+function bringShingara(money) {
+    console.log('Mama shingara den');
+    console.log(money);
+}
+
+
+// bringShingara(123);
+
+
+function sum(a, b, c, d, e) {
+    let sum = a + b + c + d + e;
+    console.log(a, b, c, d, e)
+    console.log(sum);
+}
+
+sum(1, 2, 3, 4, 5)
